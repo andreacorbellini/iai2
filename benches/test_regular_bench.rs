a5560190ebe41e98cf78ac2ffe9a63d1a9284c73
Andrea Corbellini <corbellini.andrea@gmail.com>
2025-09-25 15:09:11 -0700

    Add two new test benchmarks to better exercise RAM access

--- benches/test_regular_bench.rs
+++ benches/test_regular_bench.rs
@@ -19,4 +19,30 @@ fn bench_fibonacci_long() -> u64 {
     fibonacci(black_box(30))
 }
 
-iai::main!(bench_empty, bench_fibonacci, bench_fibonacci_long);
+fn bench_binary_search() -> usize {
+    const LEN: usize = 10 * 1024;
+    static LARGE_ARRAY: [u64; LEN] = const {
+        let mut array = [0; LEN];
+        let mut i = 0;
+        while i < LEN {
+            array[i] = i as u64;
+            i += 1;
+        }
+        array
+    };
+
+    black_box(&LARGE_ARRAY).binary_search(&black_box(123)).expect("number not found")
+}
+
+fn bench_binary_search_with_allocation() -> usize {
+    const LEN: usize = 10 * 1024;
+    let mut vec = Vec::with_capacity(LEN);
+
+    for i in 0..LEN {
+        black_box(&mut vec).push(black_box(i));
+    }
+
+    black_box(&vec).binary_search(&black_box(123)).expect("number not found")
+}
+
+iai::main!(bench_empty, bench_fibonacci, bench_fibonacci_long, bench_binary_search, bench_binary_search_with_allocation);
